7bef22ceca9fd68ca43a31404a4c42a2099f8adb
Marcel Mravec <marcel.mravec@evolwings.net>
2026-09-18 20:13:10 +0200

    Initial Commit

---
+++ IceTime/IceTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceTimeApp.swift
+//  IceTime
+//
+//  Created by Marcel Mravec on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct IceTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
